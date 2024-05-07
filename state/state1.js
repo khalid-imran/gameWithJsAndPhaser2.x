cda4021615a80161a846fac6f4332f0e79c86630
Khalid Imran <ekhalid090@gmail.com>
2024-05-08 00:51:31 +0600

    init game with js and phaser

---
+++ state/state1.js
@@ -0,0 +1,7 @@
+let  gameObject = {};
+gameObject.state1 = function () {};
+gameObject.state1.prototype = {
+    preload: function () {},
+    create: function () {},
+    update: function () {},
+}
